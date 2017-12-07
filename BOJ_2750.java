41a6eb50793fe15020fc0809865a1772061a9558
Zooon <33369797+zoonvivor@users.noreply.github.com>
2017-12-07 18:28:55 +0900

    수 정렬하기

---
+++ BOJ_2750.java
@@ -0,0 +1,21 @@
+import java.io.FileInputStream;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+
+		Scanner sc = new Scanner(new FileInputStream("input.txt"));
+		// Scanner sc1 = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		for (int i = 0; i < n; i++) {
+			arr[i] = sc.nextInt();
+		}
+		Arrays.sort(arr);
+
+		for (int i = 0; i < n; i++) {
+			System.out.println(arr[i]);
+		}
+	}
+}
