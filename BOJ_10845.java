e30eb441b818c5182a0023c3866e130ef35d94d6
Zooon <33369797+zoonvivor@users.noreply.github.com>
2017-12-07 18:07:50 +0900

    큐

---
+++ BOJ_10845.java
@@ -0,0 +1,75 @@
+import java.io.FileInputStream;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+
+		// Scanner sc = new Scanner(System.in);
+		Scanner sc = new Scanner(new FileInputStream("input.txt"));
+		int n = sc.nextInt();
+		sc.nextLine();
+
+		Queue q = new LinkedList();
+		
+		int x;
+		int last = 0;
+		for (int i = 0; i < n; i++) {
+			String str = sc.nextLine();
+
+			String instruction[] = str.split(" ");
+
+			switch (instruction[0]) {
+			case "pop":
+				if (q.isEmpty()) {
+					System.out.println("-1");
+				} else {
+					System.out.println(q.poll()); // 제거 하며 읽기
+				}
+
+				break;
+
+			case "push":
+				x = Integer.parseInt(instruction[1]);
+				last = x;
+				q.offer(x);
+				break;
+
+			case "size":
+				System.out.println(q.size());
+				break;
+
+			case "empty":
+				if (q.isEmpty()) {
+					System.out.println("1");
+				} else {
+					System.out.println("0");
+				}
+				break;
+
+			case "front":
+				if (q.isEmpty()) {
+					System.out.println(-1);
+				} else {
+					System.out.println(q.peek());
+				}
+				break;
+
+			case "back":
+				if (q.isEmpty()) {
+					System.out.println(-1);
+				} else {
+					System.out.println(last);
+				}
+				break;
+
+			}
+
+		}
+
+	}
+
+}
