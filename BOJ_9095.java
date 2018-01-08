3317bd6b338a816861313ce52e74552c6ad3d5be
Zooon <33369797+zoonvivor@users.noreply.github.com>
2018-01-08 20:39:31 +0900

    1,2,3 더하기

--- BOJ_9095.java
+++ BOJ_9095.java
@@ -1,17 +1,32 @@
 import java.io.FileInputStream;
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
-public class Main {
+class Main {
 	public static void main(String[] args) throws Exception {
-
 		Scanner sc = new Scanner(new FileInputStream("input.txt"));
-		// Scanner sc1 = new Scanner(System.in);
-		int n = sc.nextInt();
-		int[] arr = new int[n];
-		for(int i=0; i<n; i++){
-			arr[i] = sc.nextInt();
-		}
+		// Scanner sc = new Scanner(System.in);
 
+		int T = sc.nextInt();
+		for (int testcase = 0; testcase < T; testcase++) {
+			int N = sc.nextInt();
+			int[] dp = new int[N];
+			if (N == 1) {
+				dp[0] = 1;
+
+			} else if (N == 2) {
+				dp[1] = 2;
+
+			} else {
+				dp[0] = 1;
+				dp[1] = 2;
+				dp[2] = 4;
+				for (int i = 3; i < N; i++) {
+					dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+				}
+			}
+
+			System.out.println(dp[N - 1]);
+		}
 	}
+
 }
