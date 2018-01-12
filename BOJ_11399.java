d75bb16e6522c8784412f10a48233e75849a1090
Zooon <33369797+zoonvivor@users.noreply.github.com>
2018-01-12 15:24:31 +0900

    ATM

--- BOJ_11399.java
+++ BOJ_11399.java
@@ -1,25 +1,25 @@
 import java.io.FileInputStream;
 import java.util.Arrays;
+
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) throws Exception {
-
+		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(new FileInputStream("input.txt"));
-		// Scanner sc1 = new Scanner(System.in);
 
-		int x = sc.nextInt();
-		int[] arr = new int[x];
+		int N = sc.nextInt();
+		int[] arr = new int[N];
 		int sum = 0;
-
-		for (int i = 0; i < x; i++) {
+		for (int i = 0; i < N; i++) {
 			arr[i] = sc.nextInt();
 		}
 		Arrays.sort(arr);
-		for (int i = 0; i < x; i++) {
-			sum = sum + arr[i]*(x-i);
+		for (int i = 1; i < N; i++) {
+			arr[i] = arr[i] + arr[i - 1];
+			sum += arr[i];
 		}
-		System.out.println(sum);
+		System.out.println(sum + arr[0]);
 	}
 }
