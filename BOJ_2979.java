548eef203451a41b8374e5a9c1ab3bb9d21b178a
Zooon <33369797+zoonvivor@users.noreply.github.com>
2018-02-03 18:05:03 +0900

    트럭 주차

---
+++ BOJ_2979.java
@@ -0,0 +1,38 @@
+
+import java.util.Scanner;
+
+class Main {
+	public static void main(String[] args) throws Exception {
+		Scanner sc = new Scanner(System.in);
+
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		int C = sc.nextInt();
+		int[] arr = new int[100];
+		int start, end, max = 0;
+		int sum = 0;
+		for (int i = 0; i < 3; i++) {
+			start = sc.nextInt();
+			end = sc.nextInt();
+			max = Math.max(max, end);
+			for (int j = start; j < end; j++) {
+				arr[j]++;
+			}
+		}
+		for (int i = 0; i < max; i++) {
+			switch (arr[i]) {
+			case 1:
+				sum = sum + A*arr[i];
+				break;
+			case 2:
+				sum = sum + B*arr[i];
+				break;
+			case 3:
+				sum = sum + C*arr[i];
+				break;
+			}
+		}
+		System.out.println(sum);
+	}
+
+}
