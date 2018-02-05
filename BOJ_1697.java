6d4d9e7a1de15fa6ca158eadf9520c81348240e2
Zooon <33369797+zoonvivor@users.noreply.github.com>
2018-02-05 17:13:36 +0900

    숨바꼭질

--- BOJ_1697.java
+++ BOJ_1697.java
@@ -1,33 +1,43 @@
-import java.io.FileInputStream;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 
 public class Main {
+	public static void main(String args[]) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int K = sc.nextInt();
+		int[] Min = new int[100005];
+		Arrays.fill(Min, -1);
+		System.out.println(BFS(N, K, Min));
 
-	public static void main(String[] args) throws Exception {
-		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(new FileInputStream("input.txt"));
-		// Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int m = sc.nextInt();
-		int count = 0;
-		while(n!=m){
-			if(n*2 <m){
-				n = n*2;
-			}
-			else{
-				if(n<m){
-					n = n+1;
-				}
-				else{
-					n = n-1;
-				}
-			}
-			count++;
-		}
-		System.out.println(count);
-		
 	}
 
+	public static int BFS(int N, int K, int[] Min) {
+		int nextN = N;
+		int[] status = new int[3];
+		Queue<Integer> q = new LinkedList<Integer>();
+		q.add(nextN);
+		Min[nextN] = 0;
+
+		while (!q.isEmpty() && nextN != K) {
+
+			nextN = q.poll();
+			status[0] = nextN - 1;
+			status[1] = nextN + 1;
+			status[2] = nextN * 2;
+
+			for (int i = 0; i < 3; i++) {
+				if (status[i] >= 0 && status[i] <= 100000) {
+					if (Min[status[i]] == -1) {
+						q.add(status[i]);
+						Min[status[i]] = Min[nextN] + 1;
+
+					}
+				}
+			}
+		}
+		return Min[K];
+	}
 }
