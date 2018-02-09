2650709174f9caf488466ab905c7fd0980aab7c9
Zooon <33369797+zoonvivor@users.noreply.github.com>
2018-02-09 13:50:41 +0900

    로또 (for 백트래킹)

--- BOJ_6603.java
+++ BOJ_6603.java
@@ -5,14 +5,17 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 
 class Main {
+	static int N;
+	static int[] arr;
+	static int[] result;
 	public static void main(String args[]) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		while (true) {
 			String[] str = br.readLine().split(" ");
-			int N = Integer.parseInt(str[0]);
-			int[] arr = new int[N];
-			int[] result = new int[N];
+			N = Integer.parseInt(str[0]);
+			arr = new int[N];
+			result = new int[N];
 
 			if (N == 0) {
 				break;
@@ -20,29 +23,29 @@ class Main {
 			for (int i = 0; i < N; i++) {
 				arr[i] = Integer.parseInt(str[i + 1]);
 			}
-
-			for (int i = (1<<N)-1; i >=0; i--) {
-				Arrays.fill(result, 0);
-				int count = 0;
-				int bit = i;
-				for (int j = 0; bit != 0; j++, bit >>= 1) {
-					if ((1 & bit) == 0) {
-						continue;
-					}
-					result[Math.abs((N-1)-j)] = 1;
-					count++;
-				}
-				if (count == 6) {
-					for (int k = 0; k <N; k++) {
-						if(result[k]==1)
-						System.out.print(arr[k] + " ");
-					}
-					System.out.println();
-				}
-			}
+			DFS(0,0);
 			System.out.println();
 		}
 
-	}
 
+	}
+	public static void DFS(int start, int depth){
+		if(depth==6){
+			print();
+		}
+		for(int i=start; i<N; i++){
+		result[i] = 1;
+		DFS(i+1,depth+1);
+		result[i] = 0;
+		}
+		
+	}
+	public static void print(){
+		for(int i=0; i<N; i++){
+			if(result[i]==1)
+			System.out.print(arr[i]+" ");
+		}
+		System.out.println();
+	}
+	
 }
